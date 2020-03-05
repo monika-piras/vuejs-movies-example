5d4d7cf961d81aa33d233836b09a41d498de5fe0
Monica Piras <monica.piras.info@gmail.com>
2020-03-05 12:54:21 +0100

    update img row2

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -32,26 +32,26 @@
   <div class="row">
     <div class="col-sm">
       <ul class="list-group">
-        <li class="list-group-item"><img src="../../public/img/bee.jpg"></li>
-        <li class="list-group-item"><img src="../../public/img/bee.jpg"></li>
+        <li class="list-group-item"><img src="../../public/img/strawberry.jpg"></li>
+        <li class="list-group-item"><img src="../../public/img/strawberry.jpg"></li>
       </ul>
     </div>
     <div class="col-sm">
       <ul class="list-group">
-        <li class="list-group-item"><img src="../../public/img/dock.jpg"></li>
-        <li class="list-group-item"><img src="../../public/img/dock.jpg"></li>
+        <li class="list-group-item"><img src="../../public/img/trees.jpg"></li>
+        <li class="list-group-item"><img src="../../public/img/trees.jpg"></li>
       </ul>
     </div>
     <div class="col-sm">
       <ul class="list-group">
-        <li class="list-group-item"><img src="../../public/img/duck.jpg"></li>
-        <li class="list-group-item"><img src="../../public/img/duck.jpg"></li>
+        <li class="list-group-item"><img src="../../public/img/water.jpg"></li>
+        <li class="list-group-item"><img src="../../public/img/water.jpg"></li>
       </ul>
     </div>
     <div class="col-sm">
       <ul class="list-group">
-        <li class="list-group-item"><img src="../../public/img/squirrel.jpg"></li>
-        <li class="list-group-item"><img src="../../public/img/squirrel.jpg"></li>
+        <li class="list-group-item"><img src="../../public/img/flower.jpg"></li>
+        <li class="list-group-item"><img src="../../public/img/flower.jpg"></li>
       </ul>
     </div>
   </div>
