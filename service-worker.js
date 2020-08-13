2a762d5869f614a074efe5e17b5342d3643ebeba
Monica Piras <monica.piras.info@gmail.com>
2020-08-13 11:02:14 +0200

    fix logs build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e51ae110c32dba0ab2e4b8c72d07a0a1.js"
+  "/vuejs-movies-example/precache-manifest.55b7b9995dc0fe96f93cb0ab1ed96a24.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-app-film"});
